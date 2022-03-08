26bc7f7c4512cd9e422e2ae41c0be9432ba03431
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-03-08 17:24:56 +0100

    Fix: Fix header widget to be more dynamic

--- front/src/views/HomeView.js
+++ front/src/views/HomeView.js
@@ -3,12 +3,16 @@ import styles from './HomeView.module.css'
 import Header from '../components/Header'
 import HeaderWidget from '../components/widgets/HeaderWidget'
 import Map from '../components/Map'
+import MuseumStateWidget from '../components/widgets/MuseumStateWidget'
+import MeteoWidget from '../components/widgets/MeteoWidget'
 
 const HomeView = () => {
+  const widgets = [(<MuseumStateWidget key={1} />), (<MeteoWidget key={2} />)]
+
   return (
     <div className={styles.maindiv}>
       <Header />
-      <HeaderWidget />
+      <HeaderWidget widgets={widgets} />
       <Map />
     </div>
   )
