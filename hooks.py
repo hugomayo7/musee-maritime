1c887d4ba30a8104009ac60c5748fa8abf5c3a5d
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-03-08 18:53:36 +0100

    feat: Move the commit verification file to 'git/hooks/' then done a python script named 'hooks.py' to ask the developper if he want to enable the verification of his commit, if yes the hook is automatically copied to the right folder to be executed by git

---
+++ hooks.py
@@ -0,0 +1,19 @@
+
+import os
+import shutil
+
+
+def main():
+    dir = os.listdir("./git/hooks/")
+    actual_dir = os.listdir(".git/hooks/")
+    for file in dir:
+        if file in actual_dir:
+            continue
+        resp = input(file + " Do you want to move this file into the git hooks directory ? [y/n] ")
+        if resp == "y":
+            shutil.copy(f"./git/hooks/{file}", f".git/hooks/{file}")
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
