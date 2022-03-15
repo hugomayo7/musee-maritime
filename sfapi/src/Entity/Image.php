b8cb9a8534b8851c3990caccf22b3f7538d1aa42
Amélien <amelien@pop-os.localdomain>
2022-03-15 10:47:26 +0100

    update: APIPlatform ready

--- sfapi/src/Entity/Image.php
+++ sfapi/src/Entity/Image.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * *@apiResource()
+ *
  * @ORM\Entity(repositoryClass=ImageRepository::class)
  */
 class Image
