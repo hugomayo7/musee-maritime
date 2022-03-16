98e0533d053448bb7276caa792f9d1752a06a2a3
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-03-16 09:58:41 +0100

    fix: Remove warning

--- front/src/components/widgets/BoatDetailTablePlanningWidget.js
+++ front/src/components/widgets/BoatDetailTablePlanningWidget.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
-import { useParams } from 'react-router-dom'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
@@ -16,7 +15,6 @@ import { Button } from '@mui/material'
 import PlanningReservationWidget from './PlanningReservationWidget'
 
 export default function BoatDetailTablePlanningWidget (props) {
-  const { id } = useParams()
   const [open, setOpen] = React.useState(false)
   const [boatReservation, setBoatReservation] = React.useState()
   const [openModal, setOpenModal] = React.useState(false)
