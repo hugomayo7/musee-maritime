d1459b8ed0d72674b1889293628272e64152a542
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-03-17 10:52:57 +0100

    feat: Starting the creation of a carousel

--- front/src/components/GalleryPhotos.js
+++ front/src/components/GalleryPhotos.js
@@ -1,34 +1,54 @@
-import React from 'react'
-import GalleryPhotoWidget from './widgets/GalleryPhotoWidget'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import React, { useState } from 'react'
+import { AutoRotatingCarousel, Slide } from "material-auto-rotating-carousel";
+import ImageList from '@mui/material/ImageList'
+import ImageListItem from '@mui/material/ImageListItem'
 
-function srcset(image, size, rows = 1, cols = 1) {
+function srcset (image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
+    srcSet: `${image}?w=${size * cols}&h=${size *
+      rows}&fit=crop&auto=format&dpr=2 2x`
+  }
 }
 
 export default function GalleryPhotos (props) {
-    return (
-      <ImageList
-      variant="quilted"
-      cols={1}
-      rowHeight={160}
-    >
-      {props?.images?.map((el, i) => (
-        // <GalleryPhotoWidget image={el} key={i} />
-        <ImageListItem key={el}>
-          <img
-            {...srcset(el, 121, 1, 1)}
-            alt={"bateau"}
-            loading="lazy"
+  const [handleOpen, setHandleOpen] = useState({ open: false })
+  const handleClick = () => {
+    setHandleOpen({ open: true })
+  }
+
+  return (
+    <>
+      <ImageList variant='quilted' cols={1} rowHeight={160}>
+        {props?.images?.map((el, i) => (
+          // <GalleryPhotoWidget image={el} key={i} />
+          <ImageListItem key={el}>
+            <img {...srcset(el, 121, 1, 1)} alt={'bateau'} loading='lazy' onClick={handleClick} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      <AutoRotatingCarousel
+        label='Get started'
+        open={handleOpen.open}
+        onClose={() => setHandleOpen({ open: false })}
+        onStart={() => setHandleOpen({ open: false })}
+        autoplay={false}
+        mobile={true}
+        style={{ position: 'absolute' }}
+      >
+        {props?.images?.map((el, i) => (
+          // <GalleryPhotoWidget image={el} key={i} />
+          <Slide
+            media={
+              <img src='http://www.icons101.com/icon_png/size_256/id_79394/youtube.png' alt='test' />
+            }
+            mediaBackgroundStyle={{ backgroundColor: "red" }}
+            style={{ backgroundColor: "red" }}
+            title='This is a very cool feature'
+            subtitle='Just using this will blow your mind.'
           />
-        </ImageListItem>
-      ))}
-    </ImageList>
+        ))}
+      </AutoRotatingCarousel>
+    </>
   )
 }
