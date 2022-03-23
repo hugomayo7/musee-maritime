e81db344f25774acab2c9289cfb92032476730af
iceberg8 <maudeh975@gmail.com>
2022-03-23 16:21:15 +0100

    update: mise en place du nouveau visuel

--- front/src/components/GalleryPhotos.js
+++ front/src/components/GalleryPhotos.js
@@ -1,3 +1,4 @@
+/*
 import React, { useState } from 'react'
 import { AutoRotatingCarousel, Slide } from "material-auto-rotating-carousel";
 import ImageList from '@mui/material/ImageList'
@@ -59,4 +60,53 @@ export default function GalleryPhotos (props) {
 
     </>
   )
+}*/
+
+
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
+
+function srcset (image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${size *
+      rows}&fit=crop&auto=format&dpr=2 2x`
+  }
 }
+
+export default function TitlebarImageList(props) {
+  return (
+    <ImageList sx={{ width: 760, height: 450}}>
+      <ImageListItem key="Subheader" cols={2}>
+      </ImageListItem>
+      {props?.images?.map((el, i) => (
+        <ImageListItem key={el.img}>
+          <img
+            {...srcset(el,121,1,1)}
+            alt={props.name[i]}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={props.name[i]}
+            subtitle=''
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${props.name[i]}`}
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
