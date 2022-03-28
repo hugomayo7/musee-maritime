fbc2447a6cdc4b2b3e69522e52896fb03f854ec1
Hugo Mayonobe <hugomayonobe@gmail.com>
2022-03-28 19:39:17 +0200

    update: change image for Joshua

--- sfapi/migrations/Version20220328173551.php
+++ sfapi/migrations/Version20220328173551.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220328155951 extends AbstractMigration
+final class Version20220328173551 extends AbstractMigration
 {
     public function getDescription(): string
     {
